64e89fe41a25ea8e9959f48f5979bd21c5c00e40
dtanner <dan@dantanner.com>
2025-08-30 17:05:56 -0500

    add project skeleton

---
+++ build.gradle.kts
@@ -0,0 +1,69 @@
+import org.gradle.api.JavaVersion.VERSION_21
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
+plugins {
+    kotlin("jvm") version "2.2.0"
+    application
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    dependencies {
+    }
+}
+
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+application {
+    mainClass = "decisionmatrix.AppKt"
+}
+
+repositories {
+    mavenCentral()
+}
+
+tasks {
+    withType<KotlinJvmCompile>().configureEach {
+        compilerOptions {
+            allWarningsAsErrors = false
+            jvmTarget.set(JVM_21)
+            freeCompilerArgs.add("-Xjvm-default=all")
+        }
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+    }
+
+    java {
+        sourceCompatibility = VERSION_21
+        targetCompatibility = VERSION_21
+    }
+}
+
+dependencies {
+    implementation(platform("org.http4k:http4k-bom:6.15.1.0"))
+    implementation("org.http4k:http4k-client-okhttp")
+    implementation("org.http4k:http4k-core")
+    implementation("org.http4k:http4k-format-kotlinx-serialization")
+    implementation("org.http4k:http4k-ops-opentelemetry")
+    implementation("org.http4k:http4k-ops-resilience4j")
+    implementation("org.http4k:http4k-server-undertow")
+    implementation("org.http4k:http4k-web-htmx")
+    testImplementation("org.http4k:http4k-testing-approval")
+    testImplementation("org.http4k:http4k-testing-hamkrest")
+    testImplementation("org.http4k:http4k-testing-kotest")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.13.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.13.3")
+    testImplementation("org.junit.platform:junit-platform-launcher:1.13.3")
+}
+
---
+++ src/main/kotlin/decisionmatrix/App.kt
@@ -0,0 +1,33 @@
+package decisionmatrix
+
+import org.http4k.core.HttpHandler
+import org.http4k.core.Method.GET
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.OK
+import org.http4k.core.then
+import org.http4k.filter.DebuggingFilters.PrintRequest
+import org.http4k.filter.OpenTelemetryMetrics
+import org.http4k.filter.OpenTelemetryTracing
+import org.http4k.filter.ServerFilters
+import org.http4k.routing.bind
+import org.http4k.routing.routes
+import org.http4k.server.Undertow
+import org.http4k.server.asServer
+
+val app: HttpHandler = routes(
+    "/ping" bind GET to {
+        Response(OK).body("pong")
+    },
+
+    )
+
+fun main() {
+    val app: HttpHandler = PrintRequest()
+        .then(ServerFilters.OpenTelemetryTracing())
+        .then(ServerFilters.OpenTelemetryMetrics.RequestCounter())
+        .then(ServerFilters.OpenTelemetryMetrics.RequestTimer()).then(app)
+
+    val server = app.asServer(Undertow(9000)).start()
+
+    println("Server started on " + server.port())
+}
