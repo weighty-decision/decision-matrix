efaefcfcdc43803b0a81094f460c62eb68fdeff4
dtanner <dan@dantanner.com>
2025-08-31 15:47:36 -0500

    use snake_case for json output

--- src/main/kotlin/decisionmatrix/Serde.kt
+++ src/main/kotlin/decisionmatrix/Serde.kt
@@ -1,5 +1,11 @@
 package decisionmatrix
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonNamingStrategy
 
-val json = Json { ignoreUnknownKeys = true }
+@OptIn(ExperimentalSerializationApi::class)
+val json = Json {
+    ignoreUnknownKeys = true
+    namingStrategy = JsonNamingStrategy.SnakeCase
+}
